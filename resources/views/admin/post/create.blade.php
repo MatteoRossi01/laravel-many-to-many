83cc3d8882877387874dce3f2b1a3cd24ac55280
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-04-12 18:25:14 +0200

    first commit

---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,41 @@
+@extends('admin.layouts.base')
+
+@section('content')
+    <div class="container">
+        <h1 class="mb-3">Inserisci un nuovo post</h1>
+        <form method="POST" action="{{ route('admin.posts.store') }}">
+            @csrf
+
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
+            </div>
+
+            <div class="mb-3">
+                <div class="form-floating">
+                    <textarea class="form-control" placeholder="Inserisci il post qui" id="content" name="content" cols="50" rows="10">{{old('content')}}</textarea>
+                    <label for="content">Post</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="category_id">Categoria</label>
+                <select class="form-select mb-3" name="category_id" id="category_id">
+                    <option value="">Seleziona la categoria</option>
+                    @foreach ($categories as $category)
+                        <option {{old('category_id') == $category->id ? 'selected': ''}} value="{{$category->id}}">{{$category->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            @foreach ($tags as $tag)
+                <div class="custom-control custom-checkbox">
+                    <input name="tags[]" type="checkbox" class="custom-control-input" id="tag_{{$tag->id}}" value={{$tag->id}} {{in_array($tag->id, old('tags', []))?'checked':''}}>
+                    <label class="custom-control-label" for="tag_{{$tag->id}}">{{$tag->name}}</label>
+                </div>
+            @endforeach
+
+            <button type="submit" class="btn btn-primary">Salva</button>
+        </form>
+    </div>
+@endsection  
